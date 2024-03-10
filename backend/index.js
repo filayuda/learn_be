4a2d0e55d5521946d995b16f7746d8f022752d56
filayuda <filayudafirmansya@gmail.com>
2024-03-10 16:00:47 +0700

    login + register-BE

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,26 @@
 import express from 'express';
-// import router from './routes/index.js';
+import dotenv from 'dotenv';
+import router from './routes/index.js';
 import dbConfig from './config/Database.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 // import Users from './models/UserModel.js';
 
+dotenv.config();
 const app = express();
 
 try {
     await dbConfig.authenticate();
     console.log("Database connected...");
-    // await Users.sync();
+    //await Users.sync();
 
 } catch (error) {
     console.log(error);
 }
 
-// app.use(express.json());
-// app.use(router);
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
 app.listen(5000, () => console.log("server running on port 5000") );
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { getUsers } from "../controllers/Users.js";
-import { Router } from "express";
+import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { refreshToken } from "../controllers/RefreshToken.js";
 
-// const router = express.Router() 
+const router = express.Router(); 
 
-Router.get('/users', getUsers);
+//GET
+router.get('/users', verifyToken, getUsers);
+router.get('/token', refreshToken);
+
+
+//POST
+router.post('/users', Register);
+router.post('/login', Login);
+
+//DELETE
+router.delete('/logout', Logout);
+
 
 export default router;
