8a738fb5750458f8771255d29a003394d2d28162
filayuda <filayudafirmansya@gmail.com>
2024-03-11 08:59:46 +0700

    register success-fe

--- frondend/src/App.js
+++ frondend/src/App.js
@@ -1,10 +1,24 @@
 import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import Register from "./components/Register";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/dashboard" element={
+          <>
+            <Navbar />
+            <Dashboard />
+          </>
+        } />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
